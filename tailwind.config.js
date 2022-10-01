af162c2a2c62e2aa15dbfbe31dea2f54ca7b9551
Cristhian-Medina <criscis.mecasa@gmail.com>
2022-10-01 00:12:05 -0500

    chore: adding eslint extends for next and adding yarn tsc validation in husky pre-commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,29 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        fuchsiaHover: '#E5016D',
+        "primary": "#FF0079",
+        "primary-dark": "#790252",
+        "foreground": "#1E1E53",
+        "background": "#01003C",
+        "background-dark": "rgba(1, 0, 60, 0.2)",
+        "error": "#FF5252",
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+      borderRadius: {
+        generalRound: '12px'
+      },
+      borderWidth: {
+        1: '1px',
+      },
+      width: {
+        smartPhone: '375px'
+      },
+    },
   },
   plugins: [],
 }
