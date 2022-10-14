9d3e8e8ff58e0f58bdc720212d4954b50de702d2
Cristhian-Medina <criscis.mecasa@gmail.com>
2022-10-14 12:36:12 -0500

    feat: add ButtonIcon to storybook

--- stories/ButtonIcon.stories.tsx
+++ stories/ButtonIcon.stories.tsx
@@ -8,8 +8,61 @@ export default {
   component: ButtonIcon,
 } as ComponentMeta<typeof ButtonIcon>
 
-export const Primary = {
+export const like = {
   args: {
-    icon: '',
+    icon: 'like',
+    label: 'Like',
+    size: 'm',
+    action: () => console.log('Like'),
+  },
+}
+
+export const trash = {
+  args: {
+    icon: 'trash',
+    size: 'm',
+    type: 'delete-round',
+    action: () => console.log('Trash'),
+  },
+}
+
+export const play = {
+  args: {
+    icon: 'play',
+    size: 'm',
+    type: 'play',
+    action: () => console.log('play'),
+  },
+}
+
+export const next = {
+  args: {
+    icon: 'next',
+    size: 'm',
+    action: () => console.log('next'),
+  },
+}
+
+export const prev = {
+  args: {
+    icon: 'prev',
+    size: 'm',
+    action: () => console.log('prev'),
+  },
+}
+
+export const doubleNext = {
+  args: {
+    icon: 'doubleNext',
+    size: 'm',
+    action: () => console.log('doubleNext'),
+  },
+}
+
+export const doublePrev = {
+  args: {
+    icon: 'doublePrev',
+    size: 'm',
+    action: () => console.log('doublePrev'),
   },
 }
